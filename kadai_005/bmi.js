0b75ec2d6250585fec0746549e41477f394bf516
tamaki-murakami <72539750+tamaki-murakami@users.noreply.github.com>
2024-08-31 10:03:48 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+// 体重：68kg
+// 身長：1.7m
+// 計算式：[体重(kg)]÷[身長(m)×身長(m)]
+
+let bodyWeight;
+bodyWeight = 68;
+
+let bodyLength
+bodyLength = 1.7;
+
+let bmi = bodyWeight / (bodyLength * bodyLength)
+
+console.log(bmi)
